4279842bdcf6a4dd56b9535c1aa5b9ff0269c94e
xqtc161 <xqtc@tutanota.com>
2023-08-17 15:04:39 +0200

    shader uwu :3 =^.^= :P bleeeh

--- src/routes/shader/hello.glsl
+++ src/routes/shader/hello.glsl
@@ -2,15 +2,16 @@
 precision highp float;
 uniform vec2 u_resolution; // Width & height of the shader
 uniform float u_time; // Time elapsed
+uniform float time;
 
 // Constants
 #define PI 3.1415925359
 #define TWO_PI 6.2831852
-#define MAX_STEPS 30 // Mar Raymarching steps
-#define MAX_DIST 50. // Max Raymarching distance
+#define MAX_STEPS 500 // Mar Raymarching steps
+#define MAX_DIST 100. // Max Raymarching distance
 #define SURF_DIST .01 // Surface Distance
 
-float GetDist(vec3 p)
+float calc_dist(vec3 p)
 {
   vec4 s = vec4(0,1,6,1); //Sphere xyz is position w is radius
   float sphereDist = length(p-s.xyz) - s.w;
@@ -19,13 +20,13 @@ float GetDist(vec3 p)
   return d;
 }
 
-float RayMarch(vec3 ro, vec3 rd)
+float rm(vec3 ro, vec3 rd)
 {
   float dO = 0.; //Distane Origin
   for(int i=0;i<MAX_STEPS;i++)
   {
     vec3 p = ro + rd * dO;
-    float ds = GetDist(p); // ds is Distance Scene
+    float ds = calc_dist(p); // ds is Distance Scene
     dO += ds;
     if(dO > MAX_DIST || ds < SURF_DIST)
       break;
@@ -33,29 +34,29 @@ float RayMarch(vec3 ro, vec3 rd)
   return dO;
 }
 
-vec3 GetNormal(vec3 p)
+vec3 normal(vec3 p)
 {
-    float d = GetDist(p); // Distance
+    float d = calc_dist(p); // Distance
     vec2 e = vec2(.01,0); // Epsilon
     vec3 n = d - vec3(
-    GetDist(p-e.xyy),
-    GetDist(p-e.yxy),
-    GetDist(p-e.yyx));
+    calc_dist(p-e.xyy),
+    calc_dist(p-e.yxy),
+    calc_dist(p-e.yyx));
 
     return normalize(n);
 }
-float GetLight(vec3 p)
+float light(vec3 p)
 {
     // Directional light
     vec3 lightPos = vec3(5.*sin(u_time),5.,5.0*cos(u_time)); // Light Position
     vec3 l = normalize(lightPos-p); // Light Vector
-    vec3 n = GetNormal(p); // Normal Vector
+    vec3 n = normal(p); // Normal Vector
 
     float dif = dot(n,l); // Diffuse light
     dif = clamp(dif,0.,1.); // Clamp so it doesnt go below 0
 
     // Shadows
-    float d = RayMarch(p+n*SURF_DIST*2., l);
+    float d = rm(p+n*SURF_DIST*2., l);
 
     if(d<length(lightPos-p)) dif *= .1;
 
@@ -70,14 +71,14 @@ void main()
     vec3 ro = vec3(0,1,0); // Ray Origin/Camera
     vec3 rd = normalize(vec3(uv.x,uv.y,1)); // Ray Direction
 
-    float d = RayMarch(ro,rd); // Distance
+    float d = rm(ro,rd); // Distance
 
     vec3 p = ro + rd * d;
-    float dif = GetLight(p); // Diffuse lighting
+    float dif = light(p); // Diffuse lighting
     d*= .2;
     vec3 color = vec3(dif);
-    //color += GetNormal(p);
-    //float color = GetLight(p);
+    //color += normal(p);
+    //float color = light(p);
 
     // Set the output color
     gl_FragColor = vec4(color,1.0);
